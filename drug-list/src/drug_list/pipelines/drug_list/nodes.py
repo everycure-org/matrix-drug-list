34068070d8087a44c6e3abe740b380313d6f805f
Marcello DeLuca <marcello-deluca@Marcellos-MacBook-Air.local>
2024-09-25 23:37:01 -0400

    add ema list generation to pipeline

--- drug-list/src/drug_list/pipelines/drug_list/nodes.py
+++ drug-list/src/drug_list/pipelines/drug_list/nodes.py
@@ -8,6 +8,8 @@ import requests
 from io import StringIO
 from typing import List
 
+
+
 def preferRXCUI(curieList:list[str], labelList:list[str]) -> tuple:
     """
     Args: 
@@ -50,15 +52,20 @@ def getCurie(name, params):
                    "&biolink_type="+
                    params['biolink_type'])
     success = False
+    failedCounts = 0
     while not success:
         try:
+            print("Resolving ", name)
             returned = (pd.read_json(StringIO(requests.get(itemRequest).text)))
             resolvedName = returned.curie
             resolvedLabel = returned.label
             success = True
         except:
             print('name resolver error')
-
+            failedCounts += 1
+        
+        if failedCounts >= 5:
+            return "Error", "Error"
     return resolvedName, resolvedLabel
 
 
@@ -129,10 +136,8 @@ def isBasicSaltOrMetalOxide(inString: str, desalting_params: dict) -> bool:
     Args:
         inString (str): name of drug
         desalting_params (dict): parameters for desalting, including inactive cation and anion names and other terms e.g. hydrate, dibasic, etc.
-    
     Returns:
         bool: if the drug is a simple salt or metal oxide (sodium chloride, potassium phosphate, etc.), return TRUE. Otherwise, return FALSE
-    
     """
 
     components = inString.strip().split()
@@ -148,13 +153,10 @@ def removeCationsAnionsAndBasicTerms(ingredientString, desalting_params):
     Args:
         ingredientString (str): name of drug ingredient
         desalting_params (dict): parameters for desalting, including inactive cation and anion names and other terms e.g. hydrate, dibasic, etc.
-
     Returns:
         str: cleaned ingredient name with salts removed. Ideally this will be just the active moiety. The full salt name is returned if it is
              a simple salt, e.g. sodium chloride or potassium phosphate.
-
     """
-
     if not isBasicSaltOrMetalOxide(ingredientString, desalting_params):
         components = ingredientString.strip().split()
         for ind,i in enumerate(components):
@@ -172,12 +174,11 @@ def split_therapy_fda(combination_therapy_name):
     """
     Args: 
         combination_therapy_name (str): full combination therapy string including delimiters.
-
+    
     Returns:
         list[str]: a list with the delimiters and whitespace stripped.
-
-    """
     
+    """
     ingList = re.split('; | ; | AND | W/ ', combination_therapy_name)
     items_list = list(set(ingList))
     items_list.sort()
@@ -189,10 +190,9 @@ def getIngredientCuries(items_list, name_resolver_params):
     Args:
         items_list(list[str]): list of active ingredients in the therapy.
         name_resolver_params(dict): parameters fed into name resolve to acquire IDs
-
     Returns:
         list[str]: list containing the best ID for each ingredient in the therapy.
-
+    
     """
     ingredientCuriesList = []
     for i in items_list:
@@ -278,6 +278,21 @@ def generate_raw_list(rawdata: pd.DataFrame, exclusions: pd.DataFrame) -> list[s
     drugList = set(drugNames).difference(exclusions_names)
     return drugList
 
+def generate_raw_ema_list(rawdata: pd.DataFrame, exclusions: pd.DataFrame) -> list[str]:
+    """
+    Args:
+        rawdata(pd.DataFrame): raw FDA data
+        exclusions(pd.DataFrame): manually curated list of exclusions from FDA list.
+    Returns:
+        list[str]: a raw list of all of the drugs in the FDA book
+    """
+    humanDrugs = rawdata[rawdata['Category']=='Human']
+    approvedDrugs = humanDrugs[humanDrugs['Authorisation status']=='Authorised']
+    drugnames = list(approvedDrugs['International non-proprietary name (INN) / common name'])
+    exclusions_names = exclusions['name']
+    drugList = set(drugnames).difference(exclusions_names)
+    return drugList
+
 
 def generate_ob_list(rawdata: pd.DataFrame, exclusions: pd.DataFrame, split_exclusions: pd.DataFrame, desalting_params: dict, name_resolver_params: dict) -> pd.DataFrame:
     """
@@ -296,3 +311,104 @@ def generate_ob_list(rawdata: pd.DataFrame, exclusions: pd.DataFrame, split_excl
     drugList = generate_raw_list(rawdata, exclusions)
     data = generate_ob_df(drugList, desalting_params, name_resolver_params, rawdata, splitExclusions)
     return data
+
+
+
+def isCombinationTherapy_ema(item: str, exclusions: list[str]) -> bool:
+    if type(item)!=float and (("," in item) or ("/" in item) or ("AND" in item)) and item not in exclusions:
+        return True
+    return False
+
+
+def split_therapy_ema(combination_therapy_name):
+    """
+    Args: 
+        combination_therapy_name (str): full combination therapy string including delimiters.
+
+    Returns:
+        list[str]: a list with the delimiters and whitespace stripped.
+
+    """
+    ingList = re.split(', | / | AND ', combination_therapy_name)
+    items_list = list(set(ingList))
+    items_list.sort()
+    return [x.strip() for x in items_list]
+
+
+
+def generate_ema_df(drugList: list[str], split_exclusions: list[str], desalting_params: dict, name_resolver_params: dict) -> pd.DataFrame:
+    Approved_EMA = []
+    combination_therapy = []
+    therapyName = []
+    name_in_ema = []
+    curie_ID = []
+    curie_label = []
+    ingredientCuriesList = []
+
+    for index, item in enumerate(list(drugList)):
+        print(index, item)
+        name_in_ema.append(item)#1
+        Approved_EMA.append("True")#2
+
+        if isCombinationTherapy_ema(item, split_exclusions):
+            item_curie_list = []
+            combination_therapy.append("True")#3
+
+            items_list = split_therapy_ema(item)
+            new_therapies = list(i for i in items_list if i not in drugList)
+            print("found new therapies: ", new_therapies)
+            for i in new_therapies:
+                drugList.add(i)
+            newIngList = list(removeCationsAnionsAndBasicTerms(i.strip(), desalting_params).strip(' ') for i in items_list) 
+            newName = '; '.join(i for i in newIngList if i is not None)
+            print("new name:", newName)
+            therapyName.append(newName) #4
+
+            curie,label = getCurie(newName, name_resolver_params)
+            preferred_curie, preferred_label = preferRXCUI(curie, label) #prefer RXCUI labels only if combination therapy.
+
+            #curie, label = preferRXCUI(getCurie(newName, name_resolver_params))
+            curie_ID.append(preferred_curie) #5
+            curie_label.append(preferred_label) #6
+
+            item_curie_list = []
+            for i in newIngList:
+                curie, label = getCurie(i, name_resolver_params)
+                item_curie_list.append(curie[0])
+            
+            ingredientCuriesList.append(item_curie_list)#7
+
+        else:
+            combination_therapy.append("False")#3
+            therapyName.append(removeCationsAnionsAndBasicTerms(item.strip(), desalting_params))#4
+            curie,label = getCurie(item, name_resolver_params)
+            curie_ID.append(curie[0])#5
+            curie_label.append(label[0])#6
+            ingredientCuriesList.append("NA")#7
+
+    data = pd.DataFrame({'single_ID':curie_ID,
+                     'ID_Label':curie_label,
+                     'Name_EMA':name_in_ema,
+                     'Therapy_Name':therapyName, 
+                     'Approved_Europe': Approved_EMA, 
+                     'Combination_Therapy':combination_therapy, 
+                     'Ingredient_IDs':ingredientCuriesList})
+    return data
+
+
+def generate_ema_list(rawdata: pd.DataFrame, ema_exclusions: pd.DataFrame, ema_split_exclusions: pd.DataFrame, desalting_params: dict, name_resolver_params: dict) -> pd.DataFrame:
+    splitExclusions = set(list(ema_split_exclusions['name']))
+    drugList = generate_raw_ema_list(rawdata, ema_exclusions)
+    data = generate_ema_df(drugList, splitExclusions, desalting_params, name_resolver_params)
+
+    return data
+
+
+
+def generate_pmda_list(rawdata: pd.DataFrame, exclusions: pd.DataFrame, split_exclusions: pd.DataFrame, desalting_params: dict, name_resolver_params: dict) -> pd.DataFrame:
+    return None
+
+
+def build_drug_list(fda_rawdata: pd.DataFrame, ) -> pd.DataFrame:
+    generate_ob_list(rawdata, exclusions, fda_ob_split_exclusions, desalting_params, name_resolver_params)
+    generate_ema_list(ema_raw_data_set, ema_exclusions, ema_split_exclusions, desalting_params, name_resolver_params)
--- drug-list/src/drug_list/pipelines/drug_list/pipeline.py
+++ drug-list/src/drug_list/pipelines/drug_list/pipeline.py
@@ -1,8 +1,3 @@
-"""
-This is a boilerplate pipeline 'generate_orange_book_list'
-generated using Kedro 0.19.8
-"""
-
 from kedro.pipeline import Pipeline, pipeline, node
 
 from . import nodes
@@ -11,16 +6,58 @@ from . import nodes
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
+            # node(
+            #     func=nodes.generate_ob_list,
+            #     inputs=
+            #             ["orange-book-products", 
+            #             "fda_exclusions", 
+            #             "fda_ob_split_exclusions",
+            #             "params:desalting_params",
+            #             "params:name_resolver_params"],
+                         
+            #     outputs= "orange_book_list",
+            #     name = "generate-orange-book-list-node"
+            # ),
+            
             node(
-                func=nodes.generate_ob_list,
+                func=nodes.generate_ema_list,
                 inputs=
-                        ["orange-book-products", 
-                        "fda_exclusions", 
-                        "fda_ob_split_exclusions",
+                        ["ema_raw_data_set", 
+                        "ema_exclusions", 
+                        "ema_split_exclusions",
                         "params:desalting_params",
-                        "params:name_resolver_params"], 
-                outputs="orange_book_list",
-                name = "generate-orange-book-list-node"
+                        "params:name_resolver_params"],
+                        #"ema_raw_data_set",
+                        #"ema_exclusions",
+                        #"ema_split_exclusions",
+                        #"pmda_raw_data_set",
+                        #"pmda_exclusions",
+                        #"pmda_split_exclusions"],
+                         
+                outputs= "ema_list",
+                name = "generate-ema-list-node"
             ),
+
+            # node(
+            #     func=nodes.generate_pmda_list,
+            #     inputs=
+            #             ["ema_raw_data_set", 
+            #             "ema_exclusions", 
+            #             "ema_split_exclusions",
+            #             "params:desalting_params",
+            #             "params:name_resolver_params"],
+            #             #"ema_raw_data_set",
+            #             #"ema_exclusions",
+            #             #"ema_split_exclusions",
+            #             #"pmda_raw_data_set",
+            #             #"pmda_exclusions",
+            #             #"pmda_split_exclusions"],
+                         
+            #     outputs= "ema_list",
+            #     name = "generate-ema-list-node"
+            # ),
+
+
         ]
     )
+
